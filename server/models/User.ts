6c32f263acef55085b31bf987922a07f9f054cd7
kamleshkumwt12th@gmail.com <kamleshkumwt12th@gmail.com>
2026-04-19 11:29:11 +0530

    Set up User model with schema, bcrypt, and JWT

---
+++ server/models/User.ts
@@ -0,0 +1,57 @@
+import{ Document, model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+    username:{
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [3, "Username must be at least 3 characters long"],
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: [6, "Email must be at least 6 characters long"],
+    },
+    password: {
+        type: String,
+        select: false,
+        required: true,
+    }
+},{
+    timestamps: true,
+})
+
+
+userSchema.index({ email: 1 });
+
+userSchema.statics.hashPassword = async function(password: string) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+userSchema.methods.isValidPassword = async function(this: IUser, password: string) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateJWT = function(this: IUser) {
+    return jwt.sign(
+        { _id: this._id, email: this.email },
+        process.env.JWT_SECRET as string,
+        { expiresIn: "7d" }
+    );
+}
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
